078de91273bf57e392aec44e30f25f8037c7fcb1
Kenny <aSmig+github@romhat.net>
2023-11-29 19:22:07 -0800

    Add a bulk status helper

--- examples/stationc.sh
+++ examples/stationc.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 help() {
     echo "Available commands: gettemp, l-band pa-power on, l-band rf-ptt on, uhf polarization left, vhf status, rotator power on, vu-tx-relay power on ...\n"
 }
 host=127.0.0.1
 
-if [ -n "$1" ]; then
-    if [ "$1" = 'help' ]; then
-        help
-        exit 0
-    fi
-    while [ -n "$1" ]; do
-        echo "$1"
-        shift
-        sleep 0.1
-    done | nc.openbsd -uw1 $host 5005
-else
-    exec nc.openbsd -uw1 $host 5005
-fi
+case "$1" in
+    "") exec nc.openbsd -uw1 $host 5005 ;;
+    "help") help ;;
+    "status") set -- "gettemp" "vhf status" "uhf status" "l-band status" "vu-tx-relay status" "rotator status" "radio-host status" ;&
+    *)
+	while [ -n "$1" ]; do
+	    echo "$1"
+	    shift
+	    sleep 0.1
+	done | nc.openbsd -uw1 $host 5005
+    ;;
+esac
